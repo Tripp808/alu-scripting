939a729b0f1a9ccca6390b9e83d0de12580324dd
Tripp808 <o.ankeli@alustudent.com>
2023-07-15 18:03:43 +0000

    final

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 
+"""
+This script queries the Reddit API and returns the number of subscribers for a given subreddit.
+If the subreddit is invalid or any error occurs, it returns an appropriate error message.
+"""
+
 import requests
 import sys
 
 def number_of_subscribers(subreddit):
+    """
+    Retrieves the number of subscribers for the given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int or str: The number of subscribers if the subreddit is valid, or an error message if invalid or an error occurs.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Custom User Agent'}  # Set a custom User-Agent header
 
